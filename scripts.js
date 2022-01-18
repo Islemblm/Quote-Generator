708457a5957ae14d32f23d4409a17278bc9265f2
islembenz <islemboualami@gmail.com>
2022-01-18 21:14:46 +0100

    Quote generator

--- scripts.js
+++ scripts.js
@@ -1,1 +1,51 @@
-window.console.log("Hello")
+
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+let apiQuote = [];
+
+// show new quote
+function newQuote() {
+  // pick a random quote
+  const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)]
+  // check if author field is blank and replace it 
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //check quote length 
+  if (quote.text.length > 100) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
+;}
+
+// Get quotes from API 
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+      const response = await fetch(apiUrl);
+      apiQuote = await response.json();
+      newQuote()
+    } catch (error) {
+        // catch error
+        console.error(error);
+    }
+}
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
+} 
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);  
+
+// On load
+getQuotes();
